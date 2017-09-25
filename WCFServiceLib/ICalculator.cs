c297de551ccb1a772737e6d9dee0f2f92d17c30e
Cyril Rat <cyril.rat@gmail.com>
2017-09-25 21:46:11 +0200

    up and running WCF Service

---
+++ WCFServiceLib/ICalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServiceLib
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface ICalculator
+    {
+
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+
+    }
+
+}
